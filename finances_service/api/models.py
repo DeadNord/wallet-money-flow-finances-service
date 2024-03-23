90ded2e0713c1a4388a35c36e9a48c9aa648924b
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-03-23 23:39:57 +0100

    add docker-compose

---
+++ finances_service/api/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
+
+# Enum for transaction types
+class TransactionType(models.TextChoices):
+    INCOME = "Income", "Income"
+    OUTCOME = "Outcome", "Outcome"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    budget_limit = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Transaction(models.Model):
+    owner = models.ForeignKey(
+        UserProfile, on_delete=models.CASCADE, related_name="transactions"
+    )
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=7, choices=TransactionType.choices)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    from_account = models.CharField(max_length=255)
+    note = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
---
+++ finances_service/api/views.py
@@ -0,0 +1,139 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .models import UserProfile
+from .serializers import TransactionSerializer
+from .services import (
+    BudgetService,
+    TransactionService,
+    ExpensesByCategoriesService,
+    TransactionsByWeekService,
+)
+
+
+class BudgetView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        budget_service = BudgetService()
+        try:
+            data = budget_service.get_user_budget(user_id)
+            return Response(data, status=status.HTTP_200_OK)
+        except UserProfile.DoesNotExist:
+            return Response(
+                {"error": "UserProfile does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class TransactionsView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        name = request.query_params.get("name")
+        start_date = request.query_params.get("start_date")
+        end_date = request.query_params.get("end_date")
+
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        transaction_service = TransactionService()
+        transactions = transaction_service.get_user_transactions(
+            user_id, name, start_date, end_date
+        )
+        serializer = TransactionSerializer(transactions, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ExpensesByCategoriesView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        service = ExpensesByCategoriesService()
+        expenses_data = service.get_expenses_by_categories(user_id)
+        if expenses_data is None:
+            return Response(
+                {"error": "UserProfile does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(expenses_data, status=status.HTTP_200_OK)
+
+
+class TransactionsByWeekView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        transactions_by_week_service = TransactionsByWeekService()
+        try:
+            data = transactions_by_week_service.get_transactions_by_week(user_id)
+            return Response(data, status=status.HTTP_200_OK)
+        except UserProfile.DoesNotExist:
+            return Response(
+                {"error": "UserProfile does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class AddTransactionView(APIView):
+    def post(self, request, *args, **kwargs):
+        user_id = request.data.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        transaction_service = TransactionService()
+
+        try:
+            transaction, errors = transaction_service.add_transaction(
+                user_id, request.data
+            )
+            if transaction:
+                return Response(
+                    TransactionSerializer(transaction).data,
+                    status=status.HTTP_201_CREATED,
+                )
+            else:
+                return Response(errors, status=status.HTTP_400_BAD_REQUEST)
+        except UserProfile.DoesNotExist:
+            return Response(
+                {"error": "UserProfile does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+
+class DeleteTransactionView(APIView):
+    def delete(self, request, id, *args, **kwargs):
+        user_id = request.query_params.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "User ID is required"}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        transaction_service = TransactionService()
+        transaction = transaction_service.delete_transaction(user_id, id)
+        if transaction is None:
+            return Response(
+                {"error": "Transaction does not exist or you do not have permission"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        return Response(
+            {"message": "Transaction deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
