33094209a4cde1051305ff867c855757b52c6426
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-03-24 23:36:29 +0100

    eslint fix

--- finances_service/api/services/TransactionsByWeekService.py
+++ finances_service/api/services/TransactionsByWeekService.py
@@ -3,30 +3,33 @@ import datetime
 from collections import defaultdict
 from ..models import Transaction, UserProfile, TransactionType
 
+
 class TransactionsByWeekService:
     def get_transactions_by_week(self, user_id):
         user_profile = UserProfile.objects.get(user__id=user_id)
-        
+
         # Получаем начало и конец текущей недели
         today = datetime.date.today()
         start_week = today - datetime.timedelta(today.weekday())
         end_week = start_week + datetime.timedelta(7)
-        
+
         # Извлекаем транзакции пользователя за текущую неделю
         transactions = Transaction.objects.filter(
-            owner=user_profile,
-            date__range=[start_week, end_week]
+            owner=user_profile, date__range=[start_week, end_week]
         )
-        
+
         # Сортируем и суммируем транзакции по дням недели
-        transactions_by_day = defaultdict(lambda: {'income': 0.00, 'outcome': 0.00})
+        transactions_by_day = defaultdict(lambda: {"income": 0.00, "outcome": 0.00})
         for trans in transactions:
             day_name = trans.date.strftime("%A")
             if trans.type == TransactionType.INCOME:
-                transactions_by_day[day_name]['income'] += trans.amount
+                transactions_by_day[day_name]["income"] += trans.amount
             else:
-                transactions_by_day[day_name]['outcome'] += trans.amount
-        
+                transactions_by_day[day_name]["outcome"] += trans.amount
+
         # Преобразуем в список для ответа
-        result = [{'name': day, 'income': data['income'], 'outcome': data['outcome']} for day, data in transactions_by_day.items()]
+        result = [
+            {"name": day, "income": data["income"], "outcome": data["outcome"]}
+            for day, data in transactions_by_day.items()
+        ]
         return result
